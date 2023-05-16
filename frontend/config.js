08c6dde395b0a8fa631b461940fad63e2f04c73a
maxwilets <mwilets@gmail.com>
2023-05-16 12:41:11 -0600

    [fIX] changing json for more files

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-// export const endpoint = `http://localhost/api/graphql`;
+// export const endpoint = `http://localhost:3000/api/graphql`;
 // export const prodEndpoint = `fill me in when we deploy`;
-export const endpoint = `https://sea-lion-app-vvkdr.ondigitalocean.app/api/graphql`;
+export const endpoint = `localhost://3000/api/graphql`;
 export const prodEndpoint = `https://sea-lion-app-vvkdr.ondigitalocean.app/api/graphql`;
 export const perPage = 4;
 export const cors = {
