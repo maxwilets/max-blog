d17a0ae68a7ce2cd43b24d8a6d37958f845a6709
maxwilets <mwilets@gmail.com>
2023-05-16 17:15:48 -0600

    reverting document

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -2,9 +2,13 @@ import Document, { Html, Head, NextScript, Main } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
   }
 
   render() {
