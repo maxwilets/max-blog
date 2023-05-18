d1e578bf65acddd6f5caa8192e91db54cfdd80fa
maxwilets <mwilets@gmail.com>
2023-05-18 16:22:09 -0600

    dev for prod build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,11 @@
 module.exports = {
   reactStrictMode: false,
-  swcMinify: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: { ignoreBuildErrors: true },
   compiler: { styledComponents: true },
-  experimental: {
-    browsersListForSwc: true,
-    legacyBrowsers: false,
-  },
 };
-
 
 // Injected content via Sentry wizard below
 
