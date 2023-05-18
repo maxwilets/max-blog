7146fe548a7bdc2737aeb6390706a0725de59e02
maxwilets <mwilets@gmail.com>
2023-05-17 20:08:01 -0600

    experimental addition to exports

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: { ignoreBuildErrors: true },
   compiler: { styledComponents: true },
+  experimental: {
+    browsersListForSwc: true,
+    legacyBrowsers: false,
+  },
 };
--- frontend/pages/portfolio.js
+++ frontend/pages/portfolio.js
@@ -4,14 +4,6 @@ import Portfolio from '../components/Portfolio';
 export default function portfolio() {
   return (
     <>
-      {/* <Head>
-        <title>Max Wilets | Web Developer Portfolio</title>
-        <meta
-          name="description"
-          content="Max Wilets's portfolio check out my resume and skills as well as recent coursework"
-          key="desc"
-        />
-      </Head> */}
       <Portfolio />;
     </>
   );
