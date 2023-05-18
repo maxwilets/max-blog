deff8e362302952834ae8d77bac74134ee6658b4
maxwilets <mwilets@gmail.com>
2023-05-18 13:02:10 -0600

    trying something

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,14 @@
 /* global __DEV__ */
-import { ApolloProvider } from '@apollo/client';
-import NProgress from 'nprogress';
-import { Router, useRouter } from 'next/router';
-import Page from '../components/Page';
-import '../components/styles/nprogress.css';
-import apolloClient from '../lib/withData';
+import { ApolloProvider } from "@apollo/client";
+import NProgress from "nprogress";
+import Router, { useRouter } from "next/router";
+import Page from "../components/Page";
+import "../components/styles/nprogress.css";
+import apolloClient from "../lib/withData";
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
