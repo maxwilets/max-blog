0fb02e63227cabc45c27c0977360d079987fde45
maxwilets <mwilets@gmail.com>
2023-05-18 14:42:49 -0600

    removing app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -21,13 +21,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-MyApp.getInitialProps = async function ({ Component, ctx }) {
-  let pageProps = {};
-  if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx);
-  }
-  pageProps.query = ctx.query;
-  return { pageProps };
-};
-
 export default MyApp;
